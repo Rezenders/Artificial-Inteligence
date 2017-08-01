c43a4ebf3ad3e67b43bbc90d9288542a3942aacc
Gustavo <gustavorezendesilva@hotmail.com>
2017-08-01 20:57:33 -0300

    ✨ Encadeamento para frente funcionando para acoes com precondições e efeitos multiplos

--- chaining/forward.cpp
+++ chaining/forward.cpp
@@ -1,29 +1,73 @@
 #include <iostream>
 #include <list>
+#include <vector>
 
-std::list< std::pair<std::string, std::string> > actions;
-std::list< std::string> state = {"limpaE", "limpaD"};
+std::vector< std::string> state = {"limpaE", "limpaD", "cueca"};
+std::vector< std::string> goal = {"sapatoE", "sapatoD", "calça", "camisa", "chapeu"};
+
 int main(){
-  actions.push_back(std::make_pair("limpaE", "meiaE"));
-  actions.push_back(std::make_pair("limpaD", "meiaD"));
-  actions.push_back(std::make_pair("meiaE", "sapatoE"));
-  actions.push_back(std::make_pair("meiaD", "sapatoD"));
+  std::vector<std::string> actions = {            "calcaMeiaE",        "calcaMeiaD",        "vesteCalça" ,      "calçaSapatoE",     "calçaSapatoD",      "vesteCamisa",                   "colocaChapeu"};
+  std::vector< std::vector< std::string> > pre_cond = {{"limpaE"},          {"limpaD"},          {"cueca"} ,        {"meiaE", "calça"},  {"meiaD", "calça"},  {"sapatoE", "sapatoD", "calça"}, {"sapatoE", "sapatoD", "calça", "camisa"}};
+  std::vector< std::vector< std::string> > effects = {{"meiaE", "limpaE"},  {"meiaD","limpaD"},  {"calça","cueca"}, {"sapatoE","meiaE"}, {"sapatoD","meiaD"}, {"camisa"},                      {"chapeu"}};
+  std::vector< std::vector< std::string> > op =      {{"pos", "neg"},       {"pos","neg"},       {"pos","neg"},     {"pos","neg"},       {"pos","neg"},       {"pos"},                          {"pos"}};
 
-  while(!state.empty()){
-    std::string current = state.front();
-    std::cout<<"Pre condição "<<current<<std::endl;
-    bool match =false;
-
-    for(std::list< std::pair<std::string, std::string> >::iterator it=actions.begin(); it!=actions.end(); ++it){
-      if(current.compare(it->first) == 0){
-        state.pop_front();
-        state.push_back(it->second);
-        match = true;
-        std::cout<<"Efeito "<<it->second<<std::endl;
+  while(true) {
+    bool aux = false;
+    for(std::vector< std::string>::iterator it=state.begin(); it!=state.end(); ++it){
+      aux = false;
+      for(std::vector< std::string>::iterator it2=goal.begin(); it2!=goal.end(); ++it2){
+        if(*it==*it2){
+          aux = true;
+          break;
+        }
+      }
+      if(!aux){
+        break;
       }
     }
-    if(match ==false){
-      state.pop_front();
+    if(aux){
+      break;
     }
+
+    bool aux2 = false;
+    for (size_t i = 0; i < pre_cond.size(); i++) {
+      for (size_t j = 0; j < pre_cond[i].size(); j++) {
+        aux2 = false;
+        for(size_t w =0; w<state.size(); ++w){
+          if(pre_cond[i][j]==state[w]){
+            aux2 = true;
+            break;
+          }
+        }
+        if(!aux2){
+          break;
+        }
+      }
+      if(aux2){//achou uma ação
+        std::cout<<actions[i]<<std::endl;
+        for (size_t z = 0; z < effects[i].size(); z++) {
+
+          if(op[i][z]=="pos"){
+            // std::cout<<"inseriu "<<std::endl;
+            state.push_back(effects[i][z]);
+          }else if(op[i][z]=="neg"){
+            // std::cout<<"deletar "<<std::endl;
+            for(std::vector< std::string>::iterator it=state.begin(); it!=state.end(); ++it){
+              if(effects[i][z]==*it){
+                state.erase(it);
+              }
+            }
+          }
+
+        }
+      }
+
+    }
+
+    std::cout<<"Estado Final ";
+    for(std::vector<std::string>::iterator it=state.begin(); it!=state.end(); ++it){
+      std::cout<<*it<<" ";
+    }
+    std::cout<<std::endl;
   }
 }
