17f29597472eec992e301d7a3c0e634ff7402afa
Lucas Franco Lima <lucasfrancolima@cliente-150-162-208-33.wireless.ufsc.br>
2018-06-09 20:13:20 -0300

    World Configuration

---
+++ Wumpus_Prolog/wumpus.pl
@@ -0,0 +1,15 @@
+%Adjacent positions
+adj(X1,Y,X2,Y) :- X2 is X1+1, X2 > 0, X2 < 5.
+adj(X1,Y,X2,Y) :- X2 is X1-1, X2 > 0, X2 < 5.
+adj(X,Y1,X,Y2) :- Y2 is Y1+1, Y2 > 0, Y2 < 5.
+adj(X,Y1,X,Y2) :- Y2 is Y1-1, Y2 > 0, Y2 < 5.
+
+%World configuration
+config_pit(3,1).
+config_pit(3,3).
+config_pit(4,4).
+config_w(1,3).
+config_g(2,3).
+
+%World rules
+config_breeze(X,Y) :- findall()
