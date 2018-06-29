8191d120cd5eb52ab43811cb9b5240925684fe01
Gustavo <gustavorezendesilva@hotmail.com>
2018-06-29 17:18:52 -0300

    art

--- Wumpus_Prolog/simple_wumpus.pl
+++ Wumpus_Prolog/simple_wumpus.pl
@@ -5,21 +5,45 @@ adj(X,Y1,X,Y2) :- Y2 is Y1+1, Y2 > 0, Y2 < 5.
 adj(X,Y1,X,Y2) :- Y2 is Y1-1, Y2 > 0, Y2 < 5.
 
 not_pit(1,1).
-not_pit(X,Y) :- not_breezy(X2,Y2), adj(X2,Y2,X,Y).
+not_pit(X,Y) :- not_breezy(X2,Y2), 
+				adj(X2,Y2,X,Y).
 
 not_wumpus(1,1).
-not_wumpus(X,Y) :- not_stenchy(X2,Y2), adj(X2,Y2,X,Y).
+not_wumpus(X,Y) :-  not_stenchy(X2,Y2), 
+						adj(X2,Y2,X,Y).
 
-sure_wumpus(X,Y,WX,WY) :- findall([X2,Y2],(adj(X,Y,X2,Y2),\+not_wumpus(X2,Y2)),R),count(R,N),N is 1,[F] = R, [WX,WY]= F. 
-sure_pit(X,Y,WX,WY) :- findall([X2,Y2],(adj(X,Y,X2,Y2),\+not_pit(X2,Y2)),R),count(R,N),N is 1,[F] = R, [WX,WY]= F. 
+sure_wumpus(X,Y,WX,WY) :-  findall([X2,Y2],(adj(X,Y,X2,Y2),\+not_wumpus(X2,Y2)),R),
+								count(R,N),
+								N is 1,
+								[F] = R, 
+								[WX,WY]= F. 
 
-possible_pit(X,Y) :- breezy(X2,Y2), adj(X2,Y2,X,Y), \+safe(X,Y), \+not_pit(X,Y).
-pit(X,Y) :- breezy(X2,Y2), adj(X2,Y2,X,Y), sure_pit(X2,Y2,X,Y).
+sure_pit(X,Y,WX,WY) :-  findall([X2,Y2], (adj(X,Y,X2,Y2),\+not_pit(X2,Y2)),R),
+							count(R,N),
+							N is 1,
+							[F] = R, 
+							[WX,WY]= F. 
 
-possible_wumpus(X,Y) :- stenchy(X2,Y2), adj(X2,Y2,X,Y), \+safe(X,Y), \+not_wumpus(X,Y).
-wumpus(X,Y) :- stenchy(X2,Y2), adj(X2,Y2,X,Y), sure_wumpus(X2,Y2,X,Y).
+possible_pit(X,Y) :- breezy(X2,Y2), 
+						adj(X2,Y2,X,Y), 
+						\+safe(X,Y), 
+						\+not_pit(X,Y).
 
-safe(X,Y) :- not_pit(X,Y), not_wumpus(X,Y).
+pit(X,Y) :- breezy(X2,Y2), 
+			adj(X2,Y2,X,Y), 
+			sure_pit(X2,Y2,X,Y).
+
+possible_wumpus(X,Y) :- stenchy(X2,Y2), 
+							adj(X2,Y2,X,Y), 
+							\+safe(X,Y), 
+							\+not_wumpus(X,Y).
+
+wumpus(X,Y) :-  stenchy(X2,Y2), 
+					adj(X2,Y2,X,Y), 
+					sure_wumpus(X2,Y2,X,Y).
+
+safe(X,Y) :- not_pit(X,Y), 
+			not_wumpus(X,Y).
 
 not_stenchy(1,1).
 not_stenchy(2,1).
